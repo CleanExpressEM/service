f56fda38b39ac15331c5f31cd31ed9859f89d552
Rapiwash <sistemarapiwash@gmail.com>
2024-06-29 13:28:26 -0500

    reconn

--- server/config/db.js
+++ server/config/db.js
@@ -1,30 +1,26 @@
 import mongoose from "mongoose";
 
+// Habilitar logs detallados
 mongoose.set("debug", true);
 
 export const connectDB = async () => {
-  const connect = async () => {
-    try {
-      await mongoose.connect(
-        "mongodb+srv://cleanExpress:6nnigIQ0eaUd6gJ8@cluster0.b92qm1v.mongodb.net/db-laundry-system?retryWrites=true&w=majority",
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          connectTimeoutMS: 60000, // 60 segundos
-          socketTimeoutMS: 60000, // 60 segundos
-          autoReconnect: true, // Reconexión automática
-          reconnectTries: Number.MAX_VALUE, // Intentos ilimitados
-          reconnectInterval: 5000, // Intervalo de reconexión de 5 segundos
-        }
-      );
-      console.log("Conexión exitosa a MongoDB");
-    } catch (error) {
-      console.error("Error al conectar a MongoDB:", error);
-      setTimeout(connect, 5000); // Intentar reconectar en 5 segundos
-    }
-  };
-
-  connect();
+  try {
+    await mongoose.connect(
+      "mongodb+srv://cleanExpress:6nnigIQ0eaUd6gJ8@cluster0.b92qm1v.mongodb.net/db-laundry-system?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 60000, // 60 segundos
+        socketTimeoutMS: 60000, // 60 segundos
+        serverSelectionTimeoutMS: 60000, // Tiempo de espera para seleccionar un servidor
+        heartbeatFrequencyMS: 10000, // Frecuencia del latido del corazón
+      }
+    );
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    setTimeout(connectDB, 5000); // Intentar reconectar en 5 segundos
+  }
 };
 
 connectDB();
