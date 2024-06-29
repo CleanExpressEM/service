2c8c0c8bcdbac0705cc2b0b687f7077125086416
Rapiwash <sistemarapiwash@gmail.com>
2024-06-29 13:30:02 -0500

    mas tiempo

--- server/config/db.js
+++ server/config/db.js
@@ -10,10 +10,11 @@ export const connectDB = async () => {
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        connectTimeoutMS: 60000, // 60 segundos
-        socketTimeoutMS: 60000, // 60 segundos
-        serverSelectionTimeoutMS: 60000, // Tiempo de espera para seleccionar un servidor
+        connectTimeoutMS: 60000, // 60 segundos para conectar
+        socketTimeoutMS: 60000, // 60 segundos para operaciones de socket
+        serverSelectionTimeoutMS: 60000, // 60 segundos para seleccionar servidor
         heartbeatFrequencyMS: 10000, // Frecuencia del latido del corazón
+        maxTimeMS: 120000, // 120 segundos para operaciones de Mongoose
       }
     );
     console.log("Conexión exitosa a MongoDB");
